6c45b1df4f0233322498ca9a715ea850c496c631
Dariusz Okoński <darek200180@gmail.com>
2023-03-15 21:05:09 +0100

    add get logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,21 @@ const User = require('../model/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 
 /**
  * @route     GET api/auth
  * @desc      Get logged in user
  * @access    Private
  */
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    return res.json(user);
+  } catch (error) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
 });
 
 /**
