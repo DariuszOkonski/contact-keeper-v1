12c7deb5f7d099b673a9f0e25aec0bc277c390da
Dariusz Okonski <darek200180@gmail.com>
2023-03-21 16:17:00 +0100

    check git

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}...`);
 });
