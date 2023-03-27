b235aa47b2c01907fe173558cb0781505a94d684
Dariusz Okoński <darek200180@gmail.com>
2023-03-27 17:40:34 +0200

    add Login and Register

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
               </Routes>
             </div>
           </div>
