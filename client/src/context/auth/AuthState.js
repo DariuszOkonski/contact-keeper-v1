c544e033856543a20a4688e80e64ba5b4831a866
Dariusz Okoński <darek200180@gmail.com>
2023-03-27 18:40:05 +0200

    add registration to backend

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,8 @@
 import { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import { CLEAR_ERRORS, REGISTER_FAIL, REGISTER_SUCCESS } from '../types';
 
 const AuthState = (props) => {
   const initialState = {
@@ -14,19 +16,47 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
-  const loadUser = () => {
-    dispatch({
-      type: 'LoadUser',
-    });
-  };
+  const loadUser = () => {};
 
   // Register User
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.post('/api/users', formData, config);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   // Login User
+  const login = () => {
+    console.log('loaduser');
+  };
 
   // Logout
+  const logout = () => {
+    console.log('logout');
+  };
 
   // Clear Errors
+  const clearErrors = () => {
+    dispatch({
+      type: CLEAR_ERRORS,
+    });
+  };
 
   return (
     <AuthContext.Provider
@@ -37,6 +67,10 @@ const AuthState = (props) => {
         user: state.user,
         error: state.error,
         loadUser,
+        register,
+        login,
+        logout,
+        clearErrors,
       }}
     >
       {props.children}
